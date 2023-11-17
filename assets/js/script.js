56f7aa58783605fe122937b9d845b7680b6cc06d
agostinoluca <agostino.luca89@gmail.com>
2023-11-17 20:04:22 +0100

    add js fuctions fix style

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,27 +9,41 @@
     L'output del prezzo finale va stampata in pagina assicurandoci sia leggibile da un essere umano (massimo due cifre decimali, per indicare centesimi sul prezzo).
     
     // TOOLS
+        -console.log
         -let/const
         -prompt
         -if/else if
         -.tofixed(2)
         ->/<
         -document.writeLn/document.getElementById
-        -.parseInt
+        -innerHTML
     //
 */
 
-// chiediamo all'utente quanti km vuole percorrere e quanti anni ha.
+// funzione aggiunta per permettere il caricamento dell'immagine di background 
+document.addEventListener('DOMContentLoaded', function() { 
 
-// calcoliamo il prezzo totale del viaggio
+    // chiediamo all'utente quanti km vuole percorrere e quanti anni ha.
+    const kmViaggio = prompt ('Ciao! Quanti KM vuoi percorrere?');
+    const etàUtente = prompt ('Inserisci qui la tua età per definire il costo del biglietto.');
 
-// applichiamo gli sconti
+    // calcoliamo il prezzo totale del viaggio
+    const kmPrice = 0.21;
+    let ticketPrice = 0.21 * kmViaggio;
 
-    // sconto minorenni
+    // calcoliamo gli sconti
+    if (etàUtente < 18) {
+        ticketPrice *= 0.8;
+        // sconto minorenni
+    } else if (etàUtente > 65) {
+        ticketPrice *= 0.6;
+        // sconto over 65
+    }
 
-    // sconto over 65
+    // approssimiamo il risultato a due cifre decimali
+    ticketPrice = ticketPrice.toFixed(2);
 
+    // stampiamo in pagina il prezzo finale del biglietto
+    document.getElementById('ticket_price').innerHTML = `Il prezzo del biglietto è di € ${ticketPrice}`;
 
-// approssimiamo il risultato a due cifre decimali
-
-// stampiamo in pagina il prezzo finale del biglietto
+});
